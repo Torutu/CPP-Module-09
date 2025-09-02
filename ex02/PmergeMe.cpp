fb243d40d77e4fbb1f27b4cdee82026c407ec22c
Waleed Alnaimi <walnaimi@c2r6p5.hive.fi>
2025-09-02 22:26:41 +0300

    done

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -2,6 +2,7 @@
 
 int PmergeMe::nbr_of_comps = 0;
 
+// ---------------- constructor and destructor ----------------
 PmergeMe::PmergeMe() {}
 PmergeMe::PmergeMe(const PmergeMe& pm) { (void)pm; }
 PmergeMe& PmergeMe::operator=(const PmergeMe& pm)
@@ -10,14 +11,15 @@ PmergeMe& PmergeMe::operator=(const PmergeMe& pm)
     return *this;
 }
 PmergeMe::~PmergeMe() {}
-
+// ---------------- Member functions ----------------
 /* Gives an index of the nth Jacobsthal number, starting from 1.
  * round((pow(2, n) + pow(-1, n - 1)) / 3) means that it starts from 0.*/
 long _jacobsthal_number(long n) { return round((pow(2, n + 1) + pow(-1, n)) / 3); }
 
-void PmergeMe::sort_vec(std::vector<int>& vec) { _merge_insertion_sort<std::vector<int> >(vec, 1); }
+void PmergeMe::sort_vec(std::vector<int>& vec) { 
+    _merge_insertion_sort<std::vector<int> >(vec, 1);
+}
 
-void PmergeMe::sort_deque(std::deque<int>& deque)
-{
+void PmergeMe::sort_deque(std::deque<int>& deque){
     _merge_insertion_sort<std::deque<int> >(deque, 1);
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -16,7 +16,17 @@
 
 class PmergeMe
 {
-  public:
+   private:
+    template <typename T>
+    void _merge_insertion_sort(T& container, int pair_level);
+    
+    template <typename T>
+    void _swap_pair(T it, int pair_level);
+
+    template <typename T>
+    void swap_pairs_by_max(T& container, int pair_level);
+
+   public:
     PmergeMe();
     PmergeMe(const PmergeMe& pm);
     PmergeMe& operator=(const PmergeMe& pm);
@@ -27,10 +37,6 @@ class PmergeMe
 
 	static int nbr_of_comps;
 
-  private:
-    template <typename T> void _merge_insertion_sort(T& container, int pair_level);
-
-    template <typename T> void _swap_pair(T it, int pair_level);
 };
 
 long _jacobsthal_number(long n);
@@ -57,146 +63,122 @@ template <typename T> void PmergeMe::_swap_pair(T it, int pair_level)
     }
 }
 
-template <typename T> void PmergeMe::_merge_insertion_sort(T& container, int pair_level)
-{
+template <typename T>
+void PmergeMe::swap_pairs_by_max(T& container, int pair_level) {
     typedef typename T::iterator Iterator;
-
     int pair_units_nbr = container.size() / pair_level;
-    if (pair_units_nbr < 2)
-        return;
-
-    /* If there is an odd pair, we ignore it during swapping.
-       It will go to the pend as the last pair. */
-    bool is_odd = pair_units_nbr % 2 == 1;
-
-    /* It's important to caluclate the end position until which we should iterate.
-       We can have a set of values like:
-       ((1 2) (3 4)) ((3 8) (2 6)) | 0
-       where there are numbers (0 in this case) which can't even form a pair.
-       Those values should be ignored. */
+    // Skip last if odd pair
+    bool is_odd = pair_units_nbr % 2 == 1; 
+    // Define iteration range (ignore leftover elements that can't form a pair)
     Iterator start = container.begin();
-    Iterator last = next(container.begin(), pair_level * (pair_units_nbr));
-    Iterator end = next(last, -(is_odd * pair_level));
-
-    /* Swap pairs of numbers, pairs, pairs of pairs etc by the biggest pair
-       number. After each swap we recurse. */
+    Iterator last = std::next(container.begin(), pair_level * pair_units_nbr);
+    Iterator end  = std::next(last, -(is_odd * pair_level));
+    // Compare adjacent pairs by their largest element and swap if needed
     int jump = 2 * pair_level;
-    for (Iterator it = start; it != end; std::advance(it, jump))
-    {
-        Iterator this_pair = next(it, pair_level - 1);
-        Iterator next_pair = next(it, pair_level * 2 - 1);
-        if (_comp(next_pair, this_pair))
-        {
+    for (Iterator it = start; it != end; std::advance(it, jump)) {
+        Iterator this_pair = std::next(it, pair_level - 1);
+        Iterator next_pair = std::next(it, pair_level * 2 - 1);
+        if (_comp(next_pair, this_pair)) {
             _swap_pair(this_pair, pair_level);
         }
     }
+}
+
+template <typename T>
+void build_main_and_pend(T& container, int pair_level,
+                         std::vector<typename T::iterator>& main,
+                         std::vector<typename T::iterator>& pend,
+                         bool is_odd, int pair_units_nbr) 
+{
+    // Take the first two pairs and put them in the main chain
+    main.push_back(std::next(container.begin(), pair_level - 1));
+    main.push_back(std::next(container.begin(), pair_level * 2 - 1));
+
+    // For the rest of the pairs: 
+    //   - put the "a" element into pend
+    //   - put the "b" element into main
+    for (int i = 4; i <= pair_units_nbr; i += 2) {
+        pend.push_back(std::next(container.begin(), pair_level * (i - 1) - 1));
+        main.push_back(std::next(container.begin(), pair_level * i - 1));
+    }
+
+    // If the number of pairs is odd, 
+    // put the leftover element into pend
+    if (is_odd) {
+        pend.push_back(std::next(container.begin(), pair_level * (pair_units_nbr - 1)));
+    }
+}
+
+template <typename Iterator>
+void insert_pend_jacobsthal(std::vector<Iterator>& main, std::vector<Iterator>& pend) {
+    int prev_jacob = _jacobsthal_number(1);  // start with first Jacobsthal number
+    int inserted = 0;                        // track how many elements have been inserted
+
+    for (int k = 2;; k++) {
+        int curr_jacob = _jacobsthal_number(k);       // next Jacobsthal number
+        int diff = curr_jacob - prev_jacob;          // how many pend elements to insert
+        if (diff > (int)pend.size())                 // stop if there aren’t enough elements
+            break;
+
+        auto pend_it = std::next(pend.begin(), diff - 1);         // point to last element in this group
+        auto bound_it = std::next(main.begin(), curr_jacob + inserted); // insertion limit in main
+
+        // Insert pend elements into main in order
+        for (int i = 0; i < diff; i++) {
+            auto idx = std::upper_bound(main.begin(), bound_it, *pend_it, _comp<Iterator>);
+            main.insert(idx, *pend_it);      // insert at correct position
+            pend_it = pend.erase(pend_it);    // remove from pend
+            std::advance(pend_it, -1);       // adjust iterator after erase
+        }
+        prev_jacob = curr_jacob;              // update previous Jacobsthal
+        inserted += diff;                      // update number of inserted elements
+    }
+}
+
+
+template <typename Iterator>
+void insert_remaining_pend(std::vector<Iterator>& main, std::vector<Iterator>& pend, bool is_odd) {
+    // Insert remaining elements from pend into main, starting from the last element
+    for (ssize_t i = pend.size() - 1; i >= 0; i--) {
+        auto curr_pend = std::next(pend.begin(), i);                        // current element to insert
+        auto curr_bound = std::next(main.begin(), main.size() - pend.size() + i + is_odd); // upper bound for insertion
+        auto idx = std::upper_bound(main.begin(), curr_bound, *curr_pend, _comp<Iterator>); // find correct position
+        main.insert(idx, *curr_pend);                                      // insert element into main
+    }
+}
+
+
+template <typename T>
+void flatten_into_container(T& container, std::vector<typename T::iterator>& main, int pair_level) {
+    std::vector<int> copy;
+    copy.reserve(container.size());  // prepare a temporary vector to store sorted elements
+
+    // Go through each iterator in main and add its corresponding pair elements to copy
+    for (auto it : main) {
+        for (int i = 0; i < pair_level; i++) {
+            auto pair_start = it;
+            std::advance(pair_start, -pair_level + i + 1);  // move to the start of the pair
+            copy.push_back(*pair_start);                    // store the element
+        }
+    }
+
+    std::copy(copy.begin(), copy.end(), container.begin()); // overwrite original container with sorted elements
+}
+
+template <typename T>
+void PmergeMe::_merge_insertion_sort(T& container, int pair_level) {
+    int pair_units_nbr = container.size() / pair_level;
+    if (pair_units_nbr < 2)
+        return;
+    bool is_odd = pair_units_nbr % 2 == 1;
+    swap_pairs_by_max(container, pair_level);
     _merge_insertion_sort(container, pair_level * 2);
 
-    /* Main contains an already sorted sequence.
-       Pend stores a yet to be sorted numbers.
-       List data structure for quick random insertion and deletion.
-       They contain iterators instead of the numbers themselves because
-       iterators + pair_level contain all the information about the stored
-       ranges of numbers. */
-    std::vector<Iterator> main;
-    std::vector<Iterator> pend;
+    std::vector<typename T::iterator> main, pend;
+    build_main_and_pend(container, pair_level, main, pend, is_odd, pair_units_nbr);
+    insert_pend_jacobsthal(main, pend);
 
-    /* Initialize the main chain with the {b1, a1}. */
-    main.insert(main.end(), next(container.begin(), pair_level - 1));
-    main.insert(main.end(), next(container.begin(), pair_level * 2 - 1));
-
-    /* Insert the rest of a's into the main chain.
-       Insert the rest of b's into the pend. */
-    for (int i = 4; i <= pair_units_nbr; i += 2)
-    {
-        pend.insert(pend.end(), next(container.begin(), pair_level * (i - 1) - 1));
-        main.insert(main.end(), next(container.begin(), pair_level * i - 1));
-    }
-
-    /* Insert an odd element to the pend, if there are any. */
-    if (is_odd)
-    {
-        pend.insert(pend.end(), next(end, pair_level - 1));
-    }
-
-    /* Insert the pend into the main in the order determined by the
-       Jacobsthal numbers. For example: 3 2 -> 5 4 -> 11 10 9 8 7 6 -> etc.
-       During insertion, main numbers serve as an upper bound for inserting numbers,
-       in order to save number of comparisons, as we know already that, for example,
-       b5 is lesser than a5, we binary search only until a5, not until the end
-       of the container.
-           We can calculate the index of the bound element. With the way I do it,
-           the index of the bound is inserted_numbers + current_jacobsthal_number. */
-    int prev_jacobsthal = _jacobsthal_number(1);
-    int inserted_numbers = 0;
-    for (int k = 2;; k++)
-    {
-        int curr_jacobsthal = _jacobsthal_number(k);
-        int jacobsthal_diff = curr_jacobsthal - prev_jacobsthal;
-		int offset = 0;
-        if (jacobsthal_diff > static_cast<int>(pend.size()))
-            break;
-        int nbr_of_times = jacobsthal_diff;
-        typename std::vector<Iterator>::iterator pend_it = next(pend.begin(), jacobsthal_diff - 1);
-        typename std::vector<Iterator>::iterator bound_it =
-            next(main.begin(), curr_jacobsthal + inserted_numbers);
-        while (nbr_of_times)
-        {
-            typename std::vector<Iterator>::iterator idx =
-                std::upper_bound(main.begin(), bound_it, *pend_it, _comp<Iterator>);
-            typename std::vector<Iterator>::iterator inserted = main.insert(idx, *pend_it);
-            nbr_of_times--;
-            pend_it = pend.erase(pend_it);
-            std::advance(pend_it, -1);
-            /* Sometimes the inserted number in inserted at the exact index of where the bound should be.
-			   When this happens, it eclipses the bound of the next pend, and it does more comparisons
-			   than it should. We need to offset when this happens. */
-            offset += (inserted - main.begin()) == curr_jacobsthal + inserted_numbers;
-			bound_it = next(main.begin(), curr_jacobsthal + inserted_numbers - offset);
-        }
-        prev_jacobsthal = curr_jacobsthal;
-        inserted_numbers += jacobsthal_diff;
-		offset = 0;
-    }
-
-    /* Insert the remaining elements in the reversed order. Here we also want to
-       perform as less comparisons as possible, so we calculate the starting bound
-       to insert pend number to be the pair of the first pend number. If the first
-       pend number is b8, the bound is a8, if the pend number is b7, the bound is a7 etc.
-       With the way I do it the index of bound is
-       size_of_main - size_of_pend + index_of_current_pend. */
-    for (ssize_t i = pend.size() - 1; i >= 0; i--)
-    {
-        typename std::vector<Iterator>::iterator curr_pend = next(pend.begin(), i);
-        typename std::vector<Iterator>::iterator curr_bound =
-            next(main.begin(), main.size() - pend.size() + i + is_odd);
-        typename std::vector<Iterator>::iterator idx =
-            std::upper_bound(main.begin(), curr_bound, *curr_pend, _comp<Iterator>);
-        main.insert(idx, *curr_pend);
-    }
-
-    /* Use copy vector to store all the numbers, in order not to overwrite the
-       original iterators. */
-    std::vector<int> copy;
-    copy.reserve(container.size());
-    for (typename std::vector<Iterator>::iterator it = main.begin(); it != main.end(); it++)
-    {
-        for (int i = 0; i < pair_level; i++)
-        {
-            Iterator pair_start = *it;
-            std::advance(pair_start, -pair_level + i + 1);
-            copy.insert(copy.end(), *pair_start);
-        }
-    }
-
-    /* Replace values in the original container. */
-    Iterator container_it = container.begin();
-    std::vector<int>::iterator copy_it = copy.begin();
-    while (copy_it != copy.end())
-    {
-        *container_it = *copy_it;
-        container_it++;
-        copy_it++;
-    }
+    insert_remaining_pend(main, pend, is_odd);
+    flatten_into_container(container, main, pair_level);
 }
 #endif
